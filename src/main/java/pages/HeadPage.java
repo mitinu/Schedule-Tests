e2360ce7c377e5945b8dc975eb75c0b6735296b1
mitinu <168318995+mitinu@users.noreply.github.com>
2024-10-26 15:43:23 +0700

    add page

--- src/main/java/pages/HeadPage.java
+++ src/main/java/pages/HeadPage.java
@@ -1,10 +1,59 @@
 package pages;
 
+import common.CONST.LOCALSTOTAGE;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import org.openqa.selenium.Alert;
+
+import static common.CONST.CONST.ITEM_WAITING_TIME;
+import static common.CONST.LOCALSTOTAGE.*;
+
 
 public class HeadPage extends BasePage{
+
     public HeadPage(WebDriver driver) {
         super(driver);
     }
+    private final By byUploadingData = By.xpath("//input[contains(@value, 'обновлениен')]");
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(ITEM_WAITING_TIME));
 
+
+    public HeadPage clikUploadingData(){
+        WebElement uploadButton = wait.until(ExpectedConditions.elementToBeClickable(byUploadingData));
+        uploadButton.click();
+        return this;
+    }
+    public HeadPage dismissAlert(){
+        Alert alert = driver.switchTo().alert();
+        alert.dismiss();
+        alert = driver.switchTo().alert();
+        alert.accept();
+        return this;
+    }
+
+    public HeadPage acceptAlert(){
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        return this;
+    }
+    protected void checkDontNullLocalStorage(String key){
+        String script = "return window.localStorage.getItem(arguments[0]);";
+        wait.until(driver -> {
+            String value = (String) ((JavascriptExecutor) driver).executeScript(script, key);
+            return value != null;
+        });
+    }
+    public HeadPage checkLocalStorage(){
+        checkDontNullLocalStorage(LOCALSTOTAGE.BASICDATA);
+        checkDontNullLocalStorage(LOCALSTOTAGE.SAVECONFIG);
+//        String value = (String) ((JavascriptExecutor) driver).executeScript(script, key);
+//        JSONObject jsonObject = new JSONObject(value);
+//        System.out.println("Значение для ключа '" + key + "': " + jsonObject);
+        return this;
+    }
 }
